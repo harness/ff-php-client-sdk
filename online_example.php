7fe2b8ac8564665428bac6f431e716c48d4c9a72
bmjen <bryan.jen@harness.io>
2022-06-28 16:46:18 -0700

    [MAINT] Improves onboarding experience

--- online_example.php
+++ online_example.php
@@ -6,7 +6,9 @@ use Harness\Client;
 use OpenAPI\Client\Model\Target;
 
 $SDK_KEY = getenv("SDK_KEY") ?: "";  // you can put your key in env variable or you can provide in the code
+$flag_name = "harnessappdemodarkmode";
 
 $client = new Client($SDK_KEY, new Target(["name" => "harness", "identifier" => "harness"]));
+$result = $client->evaluate($flag_name, false);
 
-echo "Evaluation value for flag 'flag1' with target 'harness': " . $client->evaluate("flag1", false);
+echo "Evaluation value for flag '{$flag_name}' with target 'harness': " .json_encode($result);
